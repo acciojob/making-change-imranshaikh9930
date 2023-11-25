174bd72b333a60c23d73d70eb0e59dda4efdbfb5
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-11-25 15:15:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 const makeChange = (c) => {
   // your name here
+	const pennyValue = 1;
+	const nickelValue = 5;
+	const dimeValue = 10;
+	const quarterValue = 25;
+
+	let q= 0 ;
+	let d = 0;
+	let n = 0;
+	let p = 0;
+ q = Math.floor(c / quarterValue);
+  c %= quarterValue;
+
+  
+  d = Math.floor(c / dimeValue);
+  c %= dimeValue;
+
+  
+  n = Math.floor(c / nickelValue);
+  c %= nickelValue;
+
+ 
+  p = c;
+
+  
+  const result = {
+    q: q,
+    d: d,
+    n: n,
+    p: p
+  };
+
+  return result;
 };
 
 // Do not the change the code below
